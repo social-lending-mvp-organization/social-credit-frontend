fdca90760ad81c5d72d55a3d74ccee7f9cd8e220
Souradeep Nanda <souradeep.nanda123@gmail.com>
2018-02-09 23:38:38 +0530

    Loading from dummy.json

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Panel } from 'react-bootstrap';
 
 import './css/Dashboard.css';
+import LoanHistory from './LoanHistory';
 
 function Dashboard(props) {
   const isLoggedInText = props.isLoggedIn ? 'Logged in' : 'Not logged in';
@@ -11,7 +12,9 @@ function Dashboard(props) {
       <Panel.Heading>
         <Panel.Title componentClass="h3">Dashboard {isLoggedInText}</Panel.Title>
       </Panel.Heading>
-      <Panel.Body>DASHBOARD CONTENT</Panel.Body>
+      <Panel.Body>
+        <LoanHistory isLoggedIn={props.isLoggedIn} />
+      </Panel.Body>
     </Panel>
   );
 }
