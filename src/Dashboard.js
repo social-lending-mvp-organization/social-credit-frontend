620d484afa6c15775df8ef49b301bd81a7222257
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2018-02-12 09:53:59 +0000

    Code Review: Issue19: Dashboard: Display outstanding loan. (#2)
    * Loading from dummy.json
    
    * Linked with backend
    
    * removed dummy.json
    
    * Added Facebook Login
    
    Configured Facebook SDK and other helper functions for react.
    Facebook login is added, logs userid, access token.
    Logout function and hanlder is added, logout button is needed.
    API needed to handle post request of sending userid and access token.
    
    * Adapted to new endpoint
    
    * Changed title

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,15 +3,19 @@ import PropTypes from 'prop-types';
 import { Panel } from 'react-bootstrap';
 
 import './css/Dashboard.css';
+import LoanHistory from './LoanHistory';
 
 function Dashboard(props) {
   const isLoggedInText = props.isLoggedIn ? 'Logged in' : 'Not logged in';
+  const userid = '1';
   return (
     <Panel className="dashboard-wrapper">
       <Panel.Heading>
         <Panel.Title componentClass="h3">Dashboard {isLoggedInText}</Panel.Title>
       </Panel.Heading>
-      <Panel.Body>DASHBOARD CONTENT</Panel.Body>
+      <Panel.Body>
+        <LoanHistory userid={userid} isLoggedIn={props.isLoggedIn} />
+      </Panel.Body>
     </Panel>
   );
 }
