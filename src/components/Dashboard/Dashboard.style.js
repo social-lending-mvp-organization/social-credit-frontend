b7b8775b70d6efd9696a25889186838541ebdae6
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-03-12 12:07:28 +0530

    Injected styles to Dashboard top rows

--- src/components/Dashboard/Dashboard.style.js
+++ src/components/Dashboard/Dashboard.style.js
@@ -3,6 +3,37 @@ export const scSection = {
   justifyContent: 'space-between',
   alignItems: 'start',
   width: '70%',
+  fontSize: 'inherit',
+};
+
+export const DashboardTopRow = {
+  fontSize: '2em',
+};
+
+export const DashboardHeader = {
+  fontSize: 'inherit',
+};
+
+export const DashboardSC = {
+  fontSize: 'inherit',
+};
+
+export const DashboardSecondRow = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  flexFlow: 'row',
+  fontSize: '1.5em',
+};
+
+export const DashboardMaxAmountHeader = {
+  fontSize: 'inherit',
+  flex: 1,
+};
+
+export const DashboardMaxAmount = {
+  fontSize: 'inherit',
+  textAlign: 'end',
+  flex: 1,
 };
 
 export default scSection;
