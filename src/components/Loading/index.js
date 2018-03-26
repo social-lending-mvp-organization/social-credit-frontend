beb34d64f36daf8e012913cb28454681f5f7d811
Saurav Sahu <sauravsahu.12@live.com>
2018-03-26 10:34:58 +0530

    loading and login components.

---
+++ src/components/Loading/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import loading from './loading.svg';
+
+const Callback = () => (
+  <div>
+    {/* <img src={loading} alt="loading" /> */}
+    loading...
+  </div>
+);
+
+export default Callback;
