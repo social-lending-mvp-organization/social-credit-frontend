395ebab2d623f8464921c8d1954a76f20ae2c034
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-03-26 09:24:42 +0530

    Changed twitterGraph path in constants.js

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -2,7 +2,7 @@ const facebook = {
   profilePicture: 'https://graph.facebook.com/me/picture?redirect=false',
 };
 const twitter = {
-  twitterGraph: 'http://localhost:8080/api/users/twitterGraph?screenName=',
+  twitterGraph: '/api/users/twitterGraph?screenName=',
 };
 export default facebook;
 
