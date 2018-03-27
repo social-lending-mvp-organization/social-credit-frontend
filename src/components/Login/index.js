6fddcf3f90b80e36032a6e6d3b67676af2099617
Shubham Mathur <shubham.mathur@engineer.com>
2018-03-27 13:56:13 +0530

    Design of login page updated

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,7 +3,7 @@ import { PropTypes } from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import { Navbar, Nav, NavItem, Button } from 'react-bootstrap';
 
-// import './Login.css';
+import './Login.css';
 import * as styles from './Login.style';
 
 class Login extends React.Component {
@@ -21,7 +21,7 @@ class Login extends React.Component {
 
     if (isAuthenticated) this.props.history.replace('/');
     return (
-      <div>
+      <div className="Login">
         <Navbar
           fluid
           style={styles.navBar}
@@ -42,7 +42,32 @@ class Login extends React.Component {
             </Navbar.Link>
           </Nav>
         </Navbar>
-      </div >
+
+        <div className="Login-modal">
+          <p className="Login-modal-title">A new way to apply for loans</p>
+          <div className="Login-modal-container">
+            <div className="walkthrough-steps-image" />
+            <div className="Login-steps">
+              <p>
+                Connect your social media accounts.
+              </p>
+              <p>
+                Get your scoial score.
+              </p>
+              <p>
+                Apply for loan.
+              </p>
+            </div>
+          </div>
+          <div className="Login-secured-conditions">
+            <ul>
+              <li> We never repackage personal data for sale.</li>
+              <li> We never store your logins.</li>
+              <li> We never sell or share the content of your connections.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     );
   }
 }
