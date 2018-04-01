be5f7bd41073d62617e88dc4818ea9aca922745c
Saurav Sahu <sauravsahu.12@live.com>
2018-04-01 13:00:52 +0530

    Add login button.
    Add login button.

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import { Col, Grid, Row } from 'react-bootstrap';
 
 import { signUpAuth } from '../../Auth';
 
@@ -21,35 +21,46 @@ class Login extends React.Component {
 
     if (isAuthenticated) this.props.history.replace('/');
     return (
-      <div className="Login">
-        <div className="Login-modal">
-          <div
-            className="Login-modal-title"
-          >A new way to apply for loans.
-          </div>
-          <div className="Login-modal-container">
-            <div className="walkthrough-steps-image" />
-            <ol className="Login-steps" >
-              <li className="Login-steps-item">
-                Connect your social media accounts.
-              </li>
-              <li className="Login-steps-item">
-                Get your social score.
-              </li>
-              <li className="Login-steps-item">
-                Apply for your loan.
-              </li>
-            </ol>
-          </div>
-          <div className="Login-secured-conditions">
-            <ul>
-              <li> We never repackage personal data for sale.</li>
-              <li> We never store your logins.</li>
-              <li> We never sell or share the content of your connections.</li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <Grid fluid>
+        <Row className="Login">
+          <Col className="Login-modal" lgOffset={6} lg={6}>
+            <div
+              className="Login-app"
+            >Social Credit
+            </div>
+            <div
+              className="Login-modal-title"
+            >A new way to apply for loans.
+            </div>
+            <div className="Login-modal-container">
+              <ol className="Login-steps" >
+                <li className="Login-steps-item">
+                  Connect your social media accounts.
+                </li>
+                <li className="Login-steps-item">
+                  Get your social score.
+                </li>
+                <li className="Login-steps-item">
+                  Apply for your loan.
+                </li>
+              </ol>
+            </div>
+            <div className="Login-secured-conditions">
+              <ul>
+                <li> We never repackage personal data for sale.</li>
+                <li> We never store your logins.</li>
+                <li> We never sell or share the content of your connections.</li>
+              </ul>
+            </div>
+            <button
+              className="Login-login-button"
+              onClick={() => this.login()}
+
+            >{"Let's get started"}
+            </button>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
